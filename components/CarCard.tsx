cd7af2a33c0dcabcd0a303caacd639217758ecbc
ratishjain <ratishjain6@gmail.com>
2023-09-24 01:37:55 +0530

    dialog box modal created

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -5,12 +5,14 @@ import Image from "next/image";
 import { CarProps } from "@/types";
 import CustomButton from "./CustomButton";
 import { calculateCarRent } from "@/utils";
+import { CarDetails } from ".";
 
 interface CarCardProps {
   car: CarProps;
 }
 
 const CarCard = ({ car }: CarCardProps) => {
+  const [isOpen, setIsOpen] = useState(false);
   const { city_mpg, year, make, model, transmission, drive } = car;
   const carRent = calculateCarRent(city_mpg, year);
   return (
@@ -53,10 +55,24 @@ const CarCard = ({ car }: CarCardProps) => {
           </div>
           <div className="flex flex-col justify-center items-center gap-2">
             <Image src="/gas.svg" width={20} height={20} alt="gas" />
-            <div className="text-[14px]">{city_mpg} MPG</div>
+            <p className="text-[14px]">{city_mpg} MPG</p>
           </div>
         </div>
+        <div className="car-card__btn-container">
+          <CustomButton
+            title="View More"
+            containerStyles="w-full py-[16px] rounded-full bg-primary-blue text-white"
+            textStyles="text-white text-14px leading-[17px] font-bold"
+            rightIcon="/right-arrow.svg"
+            handleClick={() => setIsOpen(true)}
+          />
+        </div>
       </div>
+      <CarDetails
+        isOpen={isOpen}
+        closeModel={() => setIsOpen(false)}
+        car={car}
+      />
     </div>
   );
 };
--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -7,6 +7,8 @@ const CustomButton = ({
   containerStyles,
   handleClick,
   btnType,
+  textStyles,
+  rightIcon,
 }: CustomButtonProps) => {
   return (
     <div>
@@ -16,7 +18,17 @@ const CustomButton = ({
         className={`custom-btn ${containerStyles}`}
         onClick={handleClick}
       >
-        <span className={`flex-1`}>{title}</span>
+        <span className={`flex-1 ${textStyles}`}>{title}</span>
+        {rightIcon && (
+          <div className="relative w-6 h-6 ml-2">
+            <Image
+              src={rightIcon}
+              alt="rightIcon"
+              fill
+              className="object-contain"
+            />
+          </div>
+        )}
       </button>
     </div>
   );
